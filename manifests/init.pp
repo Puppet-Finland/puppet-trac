31faaa9b79476768dfe4d362ca40ef8c02bd97ea
Samuli Seppänen <samuli.seppanen@gmail.com>
2014-11-20 18:35:18 +0200

    Fixes to rdoc
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,17 +21,15 @@
 # [*db_user_password*]
 #   Password for the database user.
 #
-# == Examples
-#
 # == Authors
 #
 # Samuli Seppänen <samuli.seppanen@gmail.com>
+#
 # Samuli Seppänen <samuli@openvpn.net>
 #
 # == License
 #
-# BSD-lisence
-# See file LICENSE for details
+# BSD-license. See file LICENSE for details.
 #
 class trac
 (
