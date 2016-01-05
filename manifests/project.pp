7c36891f05820188ff7ff6fce2a639a80f6e6748
Samuli Seppänen <samuli.seppanen@gmail.com>
2016-01-05 14:11:40 +0200

    Make LDAP authentication optional in trac::project also
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/project.pp
+++ manifests/project.pp
@@ -3,26 +3,45 @@
 #
 # Create a new Trac project.
 #
+# == Parameters
+#
+# [*projectname*]
+#   The name of the project. This needs to be string. No default value.
+# [*use_ldap*]
+#   Whether this Trac instance authenticates from LDAP. Valid values are true 
+#   (default) and false. Note that if this set to true, $::trac::use_ldap also 
+#   needs be true, or LDAP settings will be missing.
+# [*db_backend*]
+#   The database backend to use. Defaults to 'postgresql', which is currently 
+#   the only supported option.
+#
 define trac::project
 (
     $projectname,
+    $use_ldap = true,
     $db_backend = 'postgresql'
 )
 {
+    validate_string($projectname)
+    validate_bool($use_ldap)
+    validate_re("${db_backend}", 'postgresql')
 
-    # Fetch LDAP authentication settings from generic Trac configuration
-    $apache2_auth_lines = $::trac::config::ldapauth::apache2_ldap_auth_lines
+    if $use_ldap {
+        # Fetch LDAP authentication settings from generic Trac configuration
+        validate_array($::trac::config::ldapauth::apache2_ldap_auth_lines)
+        $apache2_auth_lines = $::trac::config::ldapauth::apache2_ldap_auth_lines
 
-    # Apache2 authentication settings (among other things) are in this file
-    file { "trac-${projectname}":
-        ensure  => present,
-        name    => "/etc/apache2/conf.d/trac-${projectname}",
-        content => template('trac/trac-project.erb'),
-        owner   => root,
-        group   => root,
-        mode    => '0644',
-        require => [ Class['apache2::install'], Class['trac::config::common'] ],
-        notify  => Class['apache2::service'],
+        # Apache2 authentication settings (among other things) are in this file
+        file { "trac-${projectname}":
+            ensure  => present,
+            name    => "/etc/apache2/conf.d/trac-${projectname}",
+            content => template('trac/trac-project.erb'),
+            owner   => root,
+            group   => root,
+            mode    => '0644',
+            require => [ Class['apache2::install'], Class['trac::config::common'] ],
+            notify  => Class['apache2::service'],
+        }
     }
 
     # We only manage permissions of the following files and directories
