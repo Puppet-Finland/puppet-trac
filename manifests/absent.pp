6100900a4fa321e4b5db20c0e332249199a14a1e
Samuli Seppänen <samuli@openvpn.net>
2018-02-02 14:13:39 +0200

    Ensure that param from apache2 class be found
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/absent.pp
+++ manifests/absent.pp
@@ -5,6 +5,8 @@
 #
 class trac::absent {
 
+    include ::apache2::params
+
     # See commit cf93da9828a3 for details
     file { "${::apache2::params::conf_d_dir}/trac-common":
         ensure => absent,
