4a8b857127ddced5320e2e8bab520bd8157ceea1
Samuli Seppänen <samuli@openvpn.net>
2018-02-02 13:15:23 +0200

    Add basic Vagrant support
    Puppet modules are installed using librarian-puppet for minimum amount of custom
    scripting.
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

---
+++ Vagrantfile
@@ -0,0 +1,21 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+
+  config.vm.define "trac" do |box|
+    box.vm.box = "puppetlabs/ubuntu-16.04-64-puppet"
+    box.vm.box_version = "1.0.0"
+    box.vm.hostname = "trac.local"
+    box.vm.network "private_network", ip: "192.168.18.100"
+    box.vm.provision "shell" do |s|
+      s.path = "vagrant/prepare.sh"
+      s.args = "trac"
+    end
+    #box.vm.provision "shell", inline: "puppet apply --modulepath /home/vagrant/modules /vagrant/vagrant/trac.pp"
+    box.vm.provider "virtualbox" do |vb|
+      vb.gui = false
+      vb.memory = 1024
+    end
+  end
+end
---
+++ vagrant/prepare.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# Prepare modulepath prior to running "puppet apply". First parameter
+# should be the name of the module calling this script. This ensures that
+# this script can be reused in other Puppet modules.
+
+if [ "$1" = "" ]; then
+    echo "ERROR: module name not given as first parameter!"
+    exit 1
+fi
+
+THIS_MODULE=$1
+CWD=`pwd`
+
+# Install dependencies
+apt-get -y install git
+/opt/puppetlabs/puppet/bin/gem install librarian-puppet
+
+# Prepare for librarian-puppet
+cd /home/vagrant
+mkdir -p modules
+cp /vagrant/vagrant/Puppetfile .
+
+# Run librarian-puppet
+/opt/puppetlabs/puppet/bin/librarian-puppet install
+
+# Copy over this in-development module to modules
+# directory
+mkdir -p "modules/${THIS_MODULE}"
+cp -r /vagrant/* "modules/${THIS_MODULE}/"
+
+cd $CWD
