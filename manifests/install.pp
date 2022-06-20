1e3fbb060c4f85970981ea1f851b70b1371d4258
Samuli Seppänen <samuli@openvpn.net>
2022-06-20 15:26:38 +0300

    Fix trivial puppet-lint issues in trac::install
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,21 +1,20 @@
 #
-# @summary
-#   Install Trac using easy_install. By default Trac will go to /usr/local.
+# @summary install Trac using easy_install.
 #
 class trac::install
 (
-    $branch
+  String $branch
 )
 {
-    include ::python::setuptools
+  include python::setuptools
 
-    exec { 'trac-easy_install':
-        command => "easy_install https://svn.edgewall.org/repos/trac/branches/${branch}",
+  exec { 'trac-easy_install':
+    command => "easy_install https://svn.edgewall.org/repos/trac/branches/${branch}",
 
-        # This file should always be present if Trac is installed 
-        # using easy_install.
-        creates => '/usr/local/bin/trac-admin',
-        path    => [ '/usr/local/bin', '/usr/bin' ],
-        require => [ Class['trac::prequisites'], Class['python::setuptools'] ],
-    }
+    # This file should always be present if Trac is installed 
+    # using easy_install.
+    creates => '/usr/local/bin/trac-admin',
+    path    => ['/usr/local/bin', '/usr/bin'],
+    require => [Class['trac::prequisites'], Class['python::setuptools']],
+  }
 }
